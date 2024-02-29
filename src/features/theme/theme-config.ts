d68a7d0e3c74b4ab888f95d62ba7eb59c2002e00
son.phm <son.pham@sentia.com>
2024-02-29 15:19:24 +0100

    update

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,10 +1,10 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Home AI Chat";
+export const AI_DESCRIPTION = "Home AI Chat is a friendly Sentia AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
 
 You have access to the following functions:
-1. create_img: You must only use the function create_img if the user asks you to create an image.`;
+1. create_img: You must only use the function create_img if the user questions have a phrase like 'create image'. The function must not be used for phrases like 'create table', 'create email'`;
 
 export const NEW_CHAT_NAME = "New chat";
